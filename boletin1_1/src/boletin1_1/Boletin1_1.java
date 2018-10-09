8c07e90fcc887c2681af815978465dabc2d1f55f
albapc <alba.perez.ce@gmail.com>
2018-10-09 16:36:07 +0200

    area triangulo

--- boletin1_1/src/boletin1_1/Boletin1_1.java
+++ boletin1_1/src/boletin1_1/Boletin1_1.java
@@ -1,21 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package boletin1_1;
 
-/**
- *
- * @author Alba
- */
+
 public class Boletin1_1 {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        float base=4, altura=3, area;
+        area= base*altura;
+        System.out.println("Área triángulo = "+area);
     }
     
 }
