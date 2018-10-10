292e648118a49f1820129c0daf42d57c2ee9a317
albapc <43620487+albapc@users.noreply.github.com>
2018-10-10 10:28:52 +0200

    Update Boletin1_1.java

--- boletin1_1/src/boletin1_1/Boletin1_1.java
+++ boletin1_1/src/boletin1_1/Boletin1_1.java
@@ -7,7 +7,7 @@ public class Boletin1_1 {
    
     public static void main(String[] args) {
         float base=4, altura=3, area;
-        area= base*altura;
+        area= base*altura/2;
         System.out.println("Área triángulo = "+area);
     }
     
