2408a4df326652e0cba3ea7877bd6a8169af9a1a
albapc <43620487+albapc@users.noreply.github.com>
2018-10-10 10:50:42 +0200

    Update Boletin1_5.java

--- boletin1_5/src/boletin1_5/Boletin1_5.java
+++ boletin1_5/src/boletin1_5/Boletin1_5.java
@@ -9,10 +9,11 @@ public class Boletin1_5 {
     
     public static void main(String[] args) {
         Scanner ler= new Scanner(System.in);
-        float metros=0f, millas_marinas=0f;
+        double metros=0f, millas_marinas=0f, 
+        final double m1=1852;
         System.out.println("Introduce el número en millas marinas");
         millas_marinas=ler.nextFloat();
-        metros= millas_marinas*1852f;
+        metros= millas_marinas*m1;
         System.out.println(millas_marinas+" millas marinas son "+metros+" metros.");
     }
     
