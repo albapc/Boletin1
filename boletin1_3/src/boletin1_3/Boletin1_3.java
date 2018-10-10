beb671f74a5d2c426692d3efe3068760e9498f65
albapc <43620487+albapc@users.noreply.github.com>
2018-10-10 10:36:51 +0200

    Update Boletin1_3.java

--- boletin1_3/src/boletin1_3/Boletin1_3.java
+++ boletin1_3/src/boletin1_3/Boletin1_3.java
@@ -9,10 +9,12 @@ public class Boletin1_3 {
     
     public static void main(String[] args) {
         Scanner ler= new Scanner (System.in);
-        float euros=0, dolares=0;
+        float euros=0f, dolares=0f, cambio=0f;
         System.out.println("Teclea la cantidad en euros:");
         euros=ler.nextFloat();
-        dolares=euros*1.14f;
+        System.out.println("Introduce el cambio);
+        cambio=ler.nextFloat();
+        dolares=euros*cambio;
         System.out.println(euros+" euros son "+dolares+" dólares.");
     }
     
